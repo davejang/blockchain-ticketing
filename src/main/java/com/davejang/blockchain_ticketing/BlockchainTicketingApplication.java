fd26a8d8a45aa80282ebac5b43e361d9cbc23c28
davejang <davejang98@naver.com>
2024-11-27 09:50:19 +0900

    init: project initialization
    spring boot setting for project

---
+++ src/main/java/com/davejang/blockchain_ticketing/BlockchainTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.davejang.blockchain_ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainTicketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockchainTicketingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davejang/blockchain_ticketing/BlockchainTicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davejang.blockchain_ticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainTicketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
