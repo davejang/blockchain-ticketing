ff6c056b085f001d55c92d9afe0ccb83b951c6c2
davejang <davejang98@naver.com>
2024-12-05 00:43:38 +0900

    refactor: design loginpage with bootstrap template
    template:SB Admin2
    https://startbootstrap.com/theme/sb-admin-2

--- src/main/java/com/davejang/blockchain_ticketing/common/security/CustomUserDetailService.java
+++ src/main/java/com/davejang/blockchain_ticketing/common/security/CustomUserDetailService.java
@@ -1,14 +1,10 @@
 package com.davejang.blockchain_ticketing.common.security;
 
-import com.davejang.blockchain_ticketing.member.domain.Member;
 import com.davejang.blockchain_ticketing.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
