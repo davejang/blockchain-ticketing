cd5ec593d81cd5f75cff3ae97394d15ee08860ef
davejang <davejang98@naver.com>
2024-12-14 11:00:19 +0900

    config: kaia properties

--- src/main/java/com/davejang/blockchain_ticketing/BlockchainTicketingApplication.java
+++ src/main/java/com/davejang/blockchain_ticketing/BlockchainTicketingApplication.java
@@ -1,11 +1,11 @@
 package com.davejang.blockchain_ticketing;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlockchainTicketingApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BlockchainTicketingApplication.class, args);
 	}
---
+++ src/main/java/com/davejang/blockchain_ticketing/common/config/PropertyConfig.java
@@ -0,0 +1,20 @@
+package com.davejang.blockchain_ticketing.common.config;
+
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+public class PropertyConfig {
+
+    @Bean(name = "kaia")
+    public PropertiesFactoryBean propertiesFactoryBean() throws Exception {
+        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
+        ClassPathResource classPathResource = new ClassPathResource("config/kaia.properties");
+
+        propertiesFactoryBean.setLocation(classPathResource);
+
+        return propertiesFactoryBean;
+    }
+}
